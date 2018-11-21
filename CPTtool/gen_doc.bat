af178029a09df5560b63d21e99c91327c06341d9
Bruno Zuada Coelho <bruno.zuadacoelho@deltares.nl>
2018-11-21 12:01:05 +0000

    edited paths for documentation
    Related to Issue [OURS-9]
    
    
    git-svn-id: https://repos.deltares.nl/repos/OURS@8 f55127e8-9a58-465b-b56e-fa2d0b06d13a

--- CPTtool/gen_doc.bat
+++ CPTtool/gen_doc.bat
@@ -1,2 +1,2 @@
-.\Python37\Scripts\sphinx-build.exe -b html .\documentation\source .\documentation\html
+..\Python37\Scripts\sphinx-build.exe -b html .\documentation\source .\documentation\html
 pause
