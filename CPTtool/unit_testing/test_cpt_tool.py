8492acec03d2a227ef616bc38f799edb8ff8e900
Eleni Smyrniou <Eleni.Smyrniou@deltares.nl>
2018-11-26 12:31:24 +0000

    Added the unit test for the cpt tool. Related to Issue [OURS-8]
    git-svn-id: https://repos.deltares.nl/repos/OURS@20 f55127e8-9a58-465b-b56e-fa2d0b06d13a

---
+++ CPTtool/unit_testing/test_cpt_tool.py
@@ -0,0 +1,33 @@
+# unit test for the cpt_module
+
+import sys
+# add the src folder to the path to search for files
+sys.path.append('../')
+import unittest
+import cpt_tool
+import numpy as np
+
+
+class TestCptTool(unittest.TestCase):
+    def setUp(self):
+        return
+
+    def test_set_key(self):
+        local_labels = ['depth', 'tip', 'friction', 'friction_nb', 'water']
+        local_dat = [1, 2, 3, 4, 6]
+        key = cpt_tool.set_key()
+        for i in range(len(local_dat)):
+            np.testing.assert_equal(key[local_labels[i]],local_dat[i])
+        return
+
+    def tearDown(self):
+      return
+
+if __name__ == '__main__':  # pragma: no cover
+    from teamcity import is_running_under_teamcity
+    if is_running_under_teamcity():
+        from teamcity.unittestpy import TeamcityTestRunner
+        runner = TeamcityTestRunner()
+    else:
+        runner = unittest.TextTestRunner()
+    unittest.main(testRunner=runner)
