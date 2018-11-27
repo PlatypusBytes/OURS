738f654fa9aa2b4d1a9b3ea13aea8e2c3dd343ef
Bruno Zuada Coelho <bruno.zuadacoelho@deltares.nl>
2018-11-27 10:35:05 +0000

    changed the test coverage file. added .coveragerc removed the modules from the report of code coverage
    Related to Issue [OURS-15]
    
    
    git-svn-id: https://repos.deltares.nl/repos/OURS@32 f55127e8-9a58-465b-b56e-fa2d0b06d13a

--- CPTtool/unit_testing/run_test_coverage.bat
+++ CPTtool/unit_testing/run_test_coverage.bat
@@ -1,4 +1,5 @@
 ..\..\Python_37\Python.exe -m coverage run -a test_cpt_tool.py
 ..\..\Python_37\Python.exe -m coverage run -a test_cpt_module.py
-..\..\Python_37\Python.exe -m coverage report test_cpt_tool.py test_cpt_module.py
-..\..\Python_37\Python.exe -m coverage html test_cpt_tool.py test_cpt_module.py
+..\..\Python_37\Python.exe -m coverage report
+..\..\Python_37\Python.exe -m coverage html 
+pause
