ea3fdb8910c25fd9e578ff53ad284c20b79980ca
Bruno Zuada Coelho <bruno.zuadacoelho@deltares.nl>
2018-11-27 10:07:58 +0000

    changed the test coverage file. removed the modules from the report of code coverage Related to Issue [OURS-15]
    git-svn-id: https://repos.deltares.nl/repos/OURS@31 f55127e8-9a58-465b-b56e-fa2d0b06d13a

--- CPTtool/unit_testing/run_test_coverage.bat
+++ CPTtool/unit_testing/run_test_coverage.bat
@@ -1,4 +1,4 @@
-..\..\Python_37\Python.exe -m coverage run test_cpt_tool.py
-..\..\Python_37\Python.exe -m coverage run test_cpt_module.py
-..\..\Python_37\Python.exe -m coverage html
-..\..\Python_37\Python.exe -m coverage report -m
+..\..\Python_37\Python.exe -m coverage run -a test_cpt_tool.py
+..\..\Python_37\Python.exe -m coverage run -a test_cpt_module.py
+..\..\Python_37\Python.exe -m coverage report test_cpt_tool.py test_cpt_module.py
+..\..\Python_37\Python.exe -m coverage html test_cpt_tool.py test_cpt_module.py
