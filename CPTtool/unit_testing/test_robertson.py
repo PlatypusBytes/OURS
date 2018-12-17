e1766d4717bf09b9182f4241c0e786b5dce1a8b7
Eleni Smyrniou <Eleni.Smyrniou@deltares.nl>
2018-12-17 11:34:49 +0000

    Created Unit tests for robertson class Related to Issue [OURS-34]
    git-svn-id: https://repos.deltares.nl/repos/OURS@61 f55127e8-9a58-465b-b56e-fa2d0b06d13a

--- CPTtool/unit_testing/test_robertson.py
+++ CPTtool/unit_testing/test_robertson.py
@@ -1,5 +1,41 @@
 from unittest import TestCase
-
+import numpy as np
+import sys
+import unittest
+# add the src folder to the path to search for files
+sys.path.append('../')
+from CPTtool.robertson import Robertson
 
 class TestRobertson(TestCase):
-    pass
+    def setUp(self):
+        from CPTtool.robertson import Robertson
+        pass
+
+#    def test_soil_types(self):
+#        self.fail()
+
+    def test_lithology(self):
+        Rb = Robertson()
+        Rb.soil_types()
+        coords_test = []
+        Qtn = [2, 2, 10, 7, 20, 100, 900, 700, 700]
+        Fr = [0.2, 9, 8, 1, 0.2, 0.5, 0.2, 3, 9]
+        lithology_test = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+        [coords_test.append([Fr[i], Qtn[i]]) for i in range(len(Fr))]
+
+        litho, coords = Robertson.lithology(Rb,Qtn, Fr)
+        np.testing.assert_array_equal(coords_test, coords)
+        np.testing.assert_array_equal(lithology_test, litho)
+        return
+
+    def tearDown(self):
+        return
+
+if __name__ == '__main__':  # pragma: no cover
+    from teamcity import is_running_under_teamcity
+    if is_running_under_teamcity():
+        from teamcity.unittestpy import TeamcityTestRunner
+        runner = TeamcityTestRunner()
+    else:
+        runner = unittest.TextTestRunner()
+        unittest.main(testRunner=runner)
