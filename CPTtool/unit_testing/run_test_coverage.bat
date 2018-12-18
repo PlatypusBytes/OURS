7fd415cd4cdc7a7ea53b027ac86feea229dbd4dd
Eleni Smyrniou <Eleni.Smyrniou@deltares.nl>
2018-12-18 10:49:57 +0000

    Solved problem with test coverage.
    git-svn-id: https://repos.deltares.nl/repos/OURS@64 f55127e8-9a58-465b-b56e-fa2d0b06d13a

--- CPTtool/unit_testing/run_test_coverage.bat
+++ CPTtool/unit_testing/run_test_coverage.bat
@@ -1,5 +1,5 @@
 ..\..\Python_37\Python.exe -m coverage run -a test_cpt_tool.py
 ..\..\Python_37\Python.exe -m coverage run -a test_cpt_module.py
+..\..\Python_37\Python.exe -m coverage run -a test_robertson.py
 ..\..\Python_37\Python.exe -m coverage report
 ..\..\Python_37\Python.exe -m coverage html 
-pause
