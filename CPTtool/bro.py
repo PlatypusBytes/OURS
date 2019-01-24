8053099f3d3eb46aefbe6ed80aaae3693d1636d0
Maarten Pronk <Maarten.Pronk@deltares.nl>
2019-01-24 11:27:55 +0000

    OURS-49 Output from BRO database now has required datastructure.
    git-svn-id: https://repos.deltares.nl/repos/OURS@113 f55127e8-9a58-465b-b56e-fa2d0b06d13a

--- CPTtool/bro.py
+++ CPTtool/bro.py
@@ -9,7 +9,9 @@ from scipy.spatial import cKDTree as KDTree
 import pickle
 import pandas as pd
 from os.path import exists, splitext
-from os import stat
+from os import stat, name
+from zipfile import ZipFile
+from tqdm import tqdm
 
 # patch module-level attribute to enable pickle to work
 # kdtree.node = kdtree.KDTree.node
@@ -20,43 +22,68 @@ fn = "/Volumes/wdmpu/bro/brocpt.xml"
 searchstring = b"<gml:featureMember>"
 columns = ["penetrationLength", "depth", "elapsedTime", "coneResistance", "correctedConeResistance", "netConeResistance", "magneticFieldStrengthX", "magneticFieldStrengthY", "magneticFieldStrengthZ", "magneticFieldStrengthTotal", "electricalConductivity",
            "inclinationEW", "inclinationNS", "inclinationX", "inclinationY", "inclinationResultant", "magneticInclination", "magneticDeclination", "localFriction", "poreRatio", "temperature", "porePressureU1", "porePressureU2", "porePressureU3", "frictionRatio"]
+req_columns = ["penetrationLength", "depth", "coneResistance", "localFriction", "frictionRatio"]
 
 ns = "{http://www.broservices.nl/xsd/cptcommon/1.1}"
 ns2 = "{http://www.broservices.nl/xsd/dscpt/1.1}"
 ns3 = "{http://www.opengis.net/gml/3.2}"
 footer = b"</gml:FeatureCollection>"
+nodata = -999999
 
-def writexml(data, i, header, footer):
-    with open("test_{}.xml".format(cpt_count + 1), "wb") as f:
-        f.write(header)
+
+def writexml(data):
+    with open("test.xml", "wb") as f:
         f.write(data)
-        f.write(footer)
 
 
 def parse_bro_xml(xml):
     root = etree.fromstring(xml)
 
+    data = {"location_x": None, "location_y": None,
+            "offset_z": None, "predrilled_z": None}
+
+    # Location
     for loc in root.iter(ns2 + "deliveredLocation"):
         for pos in loc.iter(ns3 + "pos"):
-            return pos.text.split(" ")
+            x, y = pos.text.split(" ")
+            data["location_x"] = float(x)
+            data["location_y"] = float(y)
 
-    return
-    allowed_params = []
+    # NAP offset
+    for loc in root.iter(ns + "offset"):
+        z = loc.text
+        data["offset_z"] = float(z)
+
+    # Pre drilled depth
+    for loc in root.iter(ns + "predrilledDepth"):
+        z = loc.text
+        data["predrilled_z"] = float(z)
+
+    # Find which columns are not empty
+    avail_columns = []
     for parameters in root.iter(ns + "parameters"):
         for parameter in parameters:
             if parameter.text == "ja":
-                allowed_params.append(parameter.tag[len(ns):])
+                avail_columns.append(parameter.tag[len(ns):])
+
+    # Determine if all data is available
+    meta_usable = all([x is not None for x in data.values()])
+    data_usable = all([col in avail_columns for col in req_columns])
+    if not (meta_usable and data_usable):
+        logging.warning("CPT misses required data.")
+        return None
+
+    # Parse data array, replace nodata, filter and sort
     for element in root.iter(ns + "values"):
         sar = StringIO(element.text.replace(";", "\n"))
         ar = np.loadtxt(sar, delimiter=",")
-        ar[ar == -999999] = np.nan
-        # print(ar.shape, ar)
+        ar[ar == nodata] = np.nan
         df = pd.DataFrame(ar, columns=columns)
-        df = df[allowed_params]
+        df = df[avail_columns]
         df.sort_values(by=['depth'], inplace=True)
-        # print(df)
-        df.to_csv("test_{}.csv".format(cpt_count))
-    writexml(data, i, header, footer)
+
+    data["dataframe"] = df
+
     return data
 
 
@@ -76,26 +103,62 @@ def create_index(fn, ifn, datasize):
     locations = []
     cpt_count = 0
 
-    with open(fn, "r") as f:
-        len_ss = len(searchstring)
-        # memory-map the file, size 0 means whole file
-        mm = mmap.mmap(f.fileno(), 0, prot=mmap.PROT_READ)
-        i = 0
-        while i != -1:
-            previ = i
-            i = mm.find(searchstring, i+1)
-            data = mm[previ:i]
+    ext = splitext(fn)[1]
 
-            if cpt_count == 0:
-                header = data
-                footer = b"</gml:FeatureCollection>"
-            else:
-                tdata = header+data+footer
-                if i != -1:
-                    (x, y) = parse_xml_location(tdata)
-                    locations.append((x, y, previ, i))
-            cpt_count += 1
-        mm.close()
+    # Setup progress
+    pbar = tqdm(total=datasize, unit='bytes')
+
+    # Memory map OS specifc options
+    if name == 'nt':
+        mm_options = {}
+
+
+    if ext == ".xml":
+        with open(fn, "r") as f:
+            len_ss = len(searchstring)
+            # memory-map the file, size 0 means whole file
+            with mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ) as mm:
+                i = 0
+                while i != -1:
+                    previ = i
+                    i = mm.find(searchstring, i+1)
+                    data = mm[previ:i]
+
+                    if cpt_count == 0:
+                        header = data
+                        footer = b"</gml:FeatureCollection>"
+                    else:
+                        tdata = header+data+footer
+                        if i != -1:
+                            pbar.update(i-previ)
+                            (x, y) = parse_xml_location(tdata)
+                            locations.append((x, y, previ, i))
+                    cpt_count += 1
+
+    # This won't work yet!
+    elif ext == ".zip":
+        with ZipFile(fn) as zf:
+            with zf.open("brocpt.xml") as f:
+                with mmap.mmap(f.fileno(), 0, prot=mmap.PROT_READ) as mm:
+                    i = 0
+                    while i != -1:
+                        previ = i
+                        i = mm.find(searchstring, i+1)
+                        data = mm[previ:i]
+
+                        if cpt_count == 0:
+                            header = data
+                            footer = b"</gml:FeatureCollection>"
+                        else:
+                            tdata = header+data+footer
+                            if i != -1:
+                                (x, y) = parse_xml_location(tdata)
+                                locations.append((x, y, previ, i))
+                        cpt_count += 1
+    else:
+        raise Exception("Wrong database format.")
+
+    pbar.close()
 
     # Write size and locations to file
     with open(ifn, "wb") as f:
@@ -113,7 +176,6 @@ def query_index(index, x, y, radius=1000):
 
     # Setup KDTree based on points
     npindex = np.array(index)
-    print(npindex)
     tree = KDTree(npindex[:, 0:2])
 
     # Query point and return slices
@@ -176,5 +238,6 @@ def read_bro(parameters):
 
 if __name__ == "__main__":
     input = {"BRO_data": "/Volumes/wdmpu/bro/brocpt.xml", "Source_x": 104882, "Source_y": 478455}
+    # input = {"BRO_data": "/Users/epta/Downloads/brocpt_2019.zip", "Source_x": 104882, "Source_y": 478455}
     cpts = read_bro(input)
     print(cpts)
