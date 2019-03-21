a3c5a5ee6e2264f88243a120a9c8ca65a33f3420
Bruno Zuada Coelho <bruno.zuadacoelho@deltares.nl>
2019-03-21 11:49:19 +0000

    edited test_cpt_tool.py to account changes in cpt_tool.py Related to Issue [OURS-61]
    git-svn-id: https://repos.deltares.nl/repos/OURS@167 f55127e8-9a58-465b-b56e-fa2d0b06d13a

--- CPTtool/unit_testing/test_cpt_tool.py
+++ CPTtool/unit_testing/test_cpt_tool.py
@@ -15,13 +15,13 @@ class TestCptTool(unittest.TestCase):
     def setUp(self):
         return
 
-    def test_set_key(self):
-        local_labels = ['depth', 'tip', 'friction', 'friction_nb', 'water']
-        local_dat = [1, 2, 3, 4, 6]
-        key = cpt_tool.set_key()
-        for i in range(len(local_dat)):
-            np.testing.assert_equal(key[local_labels[i]],local_dat[i])
-        return
+    # def test_set_key(self):
+    #     local_labels = ['depth', 'tip', 'friction', 'friction_nb', 'water']
+    #     local_dat = [1, 2, 3, 4, 6]
+    #     key = cpt_tool.set_key()
+    #     for i in range(len(local_dat)):
+    #         np.testing.assert_equal(key[local_labels[i]],local_dat[i])
+    #     return
 
     def tearDown(self):
         return
