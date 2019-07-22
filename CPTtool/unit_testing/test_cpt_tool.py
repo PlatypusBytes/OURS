35df912258dbaee88185b1809471417ffb489887
Maarten Pronk <Maarten.Pronk@deltares.nl>
2019-07-22 09:42:52 +0000

    OURS-86 Added tests for new output.
    git-svn-id: https://repos.deltares.nl/repos/OURS@250 f55127e8-9a58-465b-b56e-fa2d0b06d13a

--- CPTtool/unit_testing/test_cpt_tool.py
+++ CPTtool/unit_testing/test_cpt_tool.py
@@ -4,6 +4,8 @@ import sys
 import numpy as np
 from teamcity import is_running_under_teamcity
 from teamcity.unittestpy import TeamcityTestRunner
+from os.path import join, dirname
+from rtree import index
 
 # add the src folder to the path to search for files
 sys.path.append('../')
@@ -27,15 +29,45 @@ class TestCptTool(unittest.TestCase):
         return
 
 
+class TestGeoMorph(unittest.TestCase):
+    """Test reading of the GeoMorph index."""
+
+    def test_index_read(self):
+        file_idx = join(join(dirname(__file__), '../shapefiles'), 'geomorph')
+        gm_index = index.Index(file_idx)  # created by ../shapefiles/gen_geomorph_idx.py
+
+        geomorphs_nl = list(gm_index.intersection(gm_index.bounds, objects="raw"))
+
+        self.assertTrue(isinstance(geomorphs_nl[0][0], type("")))
+        self.assertTrue(isinstance(geomorphs_nl[0][1], type({})))
+        self.assertEqual(len(geomorphs_nl), 74121)
+
+
 class TestBroDb(unittest.TestCase):
     """Test creation of index and parsing of database."""
 
-    def test_database_read(self):
+    def test_database_read_circle(self):
         input = {"BRO_data": "../bro/brocpt.xml", "Source_x": 82900, "Source_y": 443351, "Radius": 100}
         cpts = bro.read_bro(input)
 
         self.assertEqual(len(cpts["circle"]["data"]), 23)
         self.assertEqual(len(list(filter(lambda x: x is None, cpts["circle"]["data"]))), 2)
+
+    def test_database_read_polygons(self):
+        input = {"BRO_data": "../bro/brocpt.xml", "Source_x": 82900, "Source_y": 443351, "Radius": 100}
+        cpts = bro.read_bro(input)
+
+        key = sorted(cpts["polygons"].keys())[0]
+
+        self.assertEqual(len(cpts["polygons"][key]["data"]), 23)
+        self.assertTrue("perc" in cpts["polygons"][key])
+        self.assertTrue(isinstance(cpts["polygons"][key]["perc"], float))
+        self.assertTrue(100. >= cpts["polygons"][key]["perc"] > 0.)
+
+        self.assertEqual(len(cpts["polygons_nl"][key]["data"]), 23)
+        self.assertTrue("count" in cpts["polygons_nl"][key])
+        self.assertTrue(isinstance(cpts["polygons_nl"][key]["count"], int))
+        self.assertTrue(cpts["polygons_nl"][key]["count"] >= 1)
 
     def test_zipdatabase_read(self):
         input = {"BRO_data": "../bro/brocpt.zip", "Source_x": 82900, "Source_y": 443351, "Radius": 100}
