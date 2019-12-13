43b99f5082084981aea86c13cd735b91494b5161
Bruno Zuada Coelho <bruno.zuadacoelho@deltares.nl>
2019-12-13 10:24:14 +0000

    Tried to sort out buildserver issues
    git-svn-id: https://repos.deltares.nl/repos/OURS@324 f55127e8-9a58-465b-b56e-fa2d0b06d13a

--- starting_point.cmd
+++ starting_point.cmd
@@ -1,5 +1,5 @@
 CALL conda env create -f env_cpttool.yml
-CALL activate ours
+CALL activate ours_env
 CALL conda env update -f env_cpttool.yml
-CALL activate ours
+CALL activate ours_env
 CALL pip install teamcity-messages==1.25
