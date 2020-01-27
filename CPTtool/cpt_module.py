fixed the computation of depth Related to Issue [OURS-114]
git-svn-id: https://repos.deltares.nl/repos/OURS@342 f55127e8-9a58-465b-b56e-fa2d0b06d13a

CPTtool/cpt_module.py
@@ -108,7 +108,8 @@ class CPT:
         cpt["dataframe"] = cpt["dataframe"].drop_duplicates(subset='penetrationLength', keep="first")
 
         # check if there is a pre_drill. if so pad the data
-        depth, cone_resistance, friction_ratio, local_friction, pore_pressure = self.define_pre_drill(cpt)
+        depth, cone_resistance, friction_ratio, local_friction, pore_pressure = self.define_pre_drill(cpt,
+                                                                                                      length_of_average_points=minimum_samples)
 
         # check quality of CPT
         # if more than minimum_ratio CPT is corrupted: discard CPT
@@ -153,7 +154,7 @@ class CPT:
         self.water = tools_utils.smooth(self.water, window_len=nb_points, lim=0)
         return
 
-    def define_pre_drill(self, cpt_BRO):
+    def define_pre_drill(self, cpt_BRO, length_of_average_points=3):
         r"""
         Checks the existence of pre-drill.
         If predrill exists it add the average value of tip, friction and friction number to the pre-drill length.
@@ -161,10 +162,10 @@ class CPT:
         If pore water pressure is measured, the pwp is assumed to be zero at surface level.
 
         :param cpt_BRO: BRO cpt dataset
+        :param length_of_average_points: number of samples of the CPT to be used to fill pre-drill
         :return: depth, tip resistance, friction number, friction, pore water pressure
         """
 
-        length_of_average_points = 3
         starting_depth = 0
         pore_pressure = None
         if float(cpt_BRO['predrilled_z']) != 0.:
@@ -193,7 +194,7 @@ class CPT:
                 pore_pressure = np.append(local_pore_pressure, cpt_BRO['dataframe']["porePressureU2"].values)
 
             # Enrich the Penetration Length
-            depth = np.append(local_depth, cpt_BRO['dataframe']['penetrationLength'].values)
+            depth = np.append(local_depth, local_depth[-1] + dicretisation + cpt_BRO['dataframe']['penetrationLength'].values)
             coneresistance = np.append(local_cone_res, cpt_BRO['dataframe']['coneResistance'].values)
             frictionratio = np.append(local_fr_ratio, cpt_BRO['dataframe']['frictionRatio'].values)
             localfriction = np.append(local_loc_fr, cpt_BRO['dataframe']['localFriction'].values)

CPTtool/unit_testing/test_cpt_module.py
@@ -40,11 +40,11 @@ class TestCptModule(unittest.TestCase):
         self.cpt.parse_bro(cpt_data, minimum_length=0.01, minimum_samples=1)
 
         # Check the equality with the pre-given lists
-        np.testing.assert_array_equal(self.cpt.tip, [2000., 2000., 2000., 1000., 2000., 3000.])
-        np.testing.assert_array_equal(self.cpt.friction, [5000., 5000., 5000., 4000., 5000., 6000.])
-        np.testing.assert_array_equal(self.cpt.friction_nbr, [0.33, 0.33, 0.33, 0.22, 0.33, 0.44])
-        np.testing.assert_array_equal(self.cpt.depth, [0, 0.5, 1., 1.5, 2., 2.5])
-        np.testing.assert_array_equal(self.cpt.NAP, [cpt_data["offset_z"] - i for i in [0, 0.5, 1., 1.5, 2., 2.5]])
+        np.testing.assert_array_equal(self.cpt.tip, np.ones(6) * self.cpt.tip)
+        np.testing.assert_array_equal(self.cpt.friction, np.ones(6) * self.cpt.friction)
+        np.testing.assert_array_equal(self.cpt.friction_nbr, np.ones(6) * self.cpt.friction_nbr)
+        np.testing.assert_array_equal(self.cpt.depth, [0, 0.5, 1, 3, 3.5, 4])
+        np.testing.assert_array_equal(self.cpt.NAP, [cpt_data["offset_z"] - i for i in [0, 0.5, 1, 3, 3.5, 4]])
         np.testing.assert_array_equal(self.cpt.water, [0., 0., 0., 0., 0., 0.])
         np.testing.assert_array_equal(self.cpt.coord, [cpt_data["location_x"], cpt_data["location_y"]])
         np.testing.assert_equal(self.cpt.name, "cpt_name")
@@ -82,11 +82,11 @@ class TestCptModule(unittest.TestCase):
 
         # Check the equality with the pre-defined values
         np.testing.assert_array_equal(self.cpt.water, pore_pressure)
-        np.testing.assert_array_equal(self.cpt.tip, [2000., 2000., 2000., 1000., 2000., 3000.])
-        np.testing.assert_array_equal(self.cpt.friction, [5000., 5000., 5000., 4000., 5000., 6000.])
-        np.testing.assert_array_equal(self.cpt.friction_nbr, [0.33, 0.33, 0.33, 0.22, 0.33, 0.44])
-        np.testing.assert_array_equal(self.cpt.depth, [0, 0.5, 1., 1.5, 2., 2.5])
-        np.testing.assert_array_equal(self.cpt.NAP, [cpt_data["offset_z"] - i for i in [0, 0.5, 1., 1.5, 2., 2.5]])
+        np.testing.assert_array_equal(self.cpt.tip, np.ones(6) * self.cpt.tip)
+        np.testing.assert_array_equal(self.cpt.friction, np.ones(6) * self.cpt.friction)
+        np.testing.assert_array_equal(self.cpt.friction_nbr, np.ones(6) * self.cpt.friction_nbr)
+        np.testing.assert_array_equal(self.cpt.depth, [0, 0.5, 1, 3, 3.5, 4])
+        np.testing.assert_array_equal(self.cpt.NAP, [cpt_data["offset_z"] - i for i in [0, 0.5, 1, 3, 3.5, 4]])
         np.testing.assert_array_equal(self.cpt.coord, [cpt_data["location_x"], cpt_data["location_y"]])
         np.testing.assert_equal(self.cpt.name, "cpt_name")
         return
@@ -617,9 +617,9 @@ class TestCptModule(unittest.TestCase):
                     "dataframe": df}
         self.cpt.parse_bro(cpt_data, minimum_length=0.01, minimum_samples=1)
 
-        np.testing.assert_array_equal(self.cpt.tip, [1500, 1000, 2000])
-        np.testing.assert_array_equal(self.cpt.friction, [3500, 3000, 4000])
-        np.testing.assert_array_equal(self.cpt.friction_nbr, [0.275, 0.22, 0.33])
+        np.testing.assert_array_equal(self.cpt.tip, np.ones(3) * self.cpt.tip)
+        np.testing.assert_array_equal(self.cpt.friction, np.ones(3) * self.cpt.friction)
+        np.testing.assert_array_equal(self.cpt.friction_nbr, np.ones(3) * self.cpt.friction_nbr)
         np.testing.assert_array_equal(self.cpt.depth, [0, 0.1, 0.2])
         np.testing.assert_array_equal(self.cpt.NAP, [cpt_data["offset_z"] - i for i in [0, 0.1, 0.2]])
         np.testing.assert_array_equal(self.cpt.water, [0., 0., 0.])
@@ -644,12 +644,12 @@ class TestCptModule(unittest.TestCase):
                     'predrilled_z': 0.}
         self.cpt.parse_bro(cpt_data, minimum_length=0.01, minimum_samples=1)
 
-        np.testing.assert_array_equal(self.cpt.tip, [1500, 1000, 2000])
-        np.testing.assert_array_equal(self.cpt.friction, [3500, 3000, 4000])
-        np.testing.assert_array_equal(self.cpt.friction_nbr, [0.275, 0.22, 0.33])
+        np.testing.assert_array_equal(self.cpt.tip, np.ones(3) * self.cpt.tip)
+        np.testing.assert_array_equal(self.cpt.friction, np.ones(3) * self.cpt.friction)
+        np.testing.assert_array_equal(self.cpt.friction_nbr, np.ones(3) * self.cpt.friction_nbr)
         np.testing.assert_array_equal(self.cpt.depth, [0, 0.1, 0.2])
         np.testing.assert_array_equal(self.cpt.NAP, [cpt_data["offset_z"] - i for i in [0, 0.1, 0.2]])
-        np.testing.assert_array_equal(self.cpt.water, [i * 1000 for i in [0.75, 0.5, 1]])
+        np.testing.assert_array_equal(self.cpt.water, [i * 1000 for i in [0.5, 0.5, 1]])
         np.testing.assert_array_equal(self.cpt.coord, [cpt_data["location_x"], cpt_data["location_y"]])
         np.testing.assert_equal(self.cpt.name, "cpt_name")
         return