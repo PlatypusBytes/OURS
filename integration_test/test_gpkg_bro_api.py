b57a4d870e4a91bf83463b8c55d71808396b2adf
brunozc <brunozuada@gmail.com>
2025-01-07 15:56:39 +0100

    Refactor integration tests to use pickled data and update setup.cfg license description

--- integration_test/test_gpkg_bro_api.py
+++ integration_test/test_gpkg_bro_api.py
@@ -1,10 +1,10 @@
 from os.path import join, dirname
 import unittest
-from CPTtool import bro
-from BroReader import read_BRO
+import pickle
 import pandas as pd
 import numpy as np
 
+from CPTtool import bro
 
 class TestBroDb(unittest.TestCase):
     """Test creation of index and parsing of database."""
@@ -14,7 +14,16 @@ class TestBroDb(unittest.TestCase):
         test_db = join(dirname(__file__), '../bro/test_v2_0_1.gpkg')
         input = {"BRO_data": test_db, "Source_x": 82900, "Source_y": 443351, "Radius": 1}
         cpts = bro.read_bro_gpkg_version(input)
-        c = read_BRO.read_cpts([input["Source_x"], input["Source_y"]], input["Radius"] * 0.001, interpret_cpt=False)
+
+        # data bro_data.pickle was created with
+        ##  from BroReader import read_BRO
+        ##  c = read_BRO.read_cpts([input["Source_x"], input["Source_y"]], input["Radius"] * 0.001, interpret_cpt=False)
+        ## with open("data_bro.pickle", "wb") as f:
+        ##     pickle.dump(c, f)
+
+        with open("./integration_test/inputs/data_bro.pickle", "rb") as f:
+            c = pickle.load(f)
+
         # compare the number of cpts read from the database and the number of cpts read from the file
         self.assertEqual(len(cpts["circle"]["data"]), len(c))
         target_cpt = cpts['circle']['data'][0]
